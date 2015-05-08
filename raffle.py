489a5dba7f9c79201c575b35c1a37e50ab20ead6
Chip Warden <chip.warden@gmail.com>
2015-05-08 16:33:57 -0500

    PEP8'd ya, bro. Sorry.

--- raffle.py
+++ raffle.py
@@ -5,6 +5,7 @@ import webbrowser
 from pythonkc_meetups import PythonKCMeetups
 from optparse import OptionParser
 
+
 def raffle_time(api_key=None, event_id=None):
     client = PythonKCMeetups(api_key=api_key)
     attendees = client.get_event_attendees(event_id)
@@ -13,13 +14,13 @@ def raffle_time(api_key=None, event_id=None):
     if winner.photo:
         webbrowser.open_new(winner.photo.url)
     print "\n\nAnd the winner is, %s \n\n" % winner.name
-    
+
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-k", "--key", help="api key for meetup.com",
-            dest="api_key", type="string")
+                      dest="api_key", type="string")
     parser.add_option("-e", "--event_id", help="event id from meetup.com",
-        dest="event_id", type="int")
+                      dest="event_id", type="int")
     options, args = parser.parse_args()
     raffle_time(api_key=options.api_key, event_id=options.event_id)
